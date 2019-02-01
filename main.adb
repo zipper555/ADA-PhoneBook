7b060759ff0050f8fedd04c59b977be40b58aeb1
SPrasad <s.prasad413@gmail.com>
2019-02-01 20:18:29 +0100

    Including time stats

--- main.adb
+++ main.adb
@@ -14,22 +14,21 @@ begin
    Put_Line("       Welcome          ");
    Put_Line("-------------------------");
    loop
-      Put_Line("1. Add 2.Delete 3.Search 4.Sort 5.Display");
+      Put_Line("1. Add 2.Delete 3.Search 4.Time Stats 5.Display");
       Get(UserOption);
       case UserOption is
       when 1 =>
          -- Add Contact
          Add_Contact;
-         null;
       when 2 =>
          -- Delete Contact
-         null;
+         Delete_Contact;
       when 3 =>
          -- Search Contact
          null;
       when 4 =>
-         -- Sort contact
-         null;
+         -- Get time stats.
+         Get_Time_Stats;
       when 5 =>
          --Display Contact
          null;
