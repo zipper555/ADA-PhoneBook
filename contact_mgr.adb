16f5f1bb488409e889a94b079624ce9e3448261c
SPrasad <s.prasad413@gmail.com>
2019-02-01 16:31:57 +0100

    Protected obj contact count

--- contact_mgr.adb
+++ contact_mgr.adb
@@ -8,52 +8,91 @@ package body Contact_Mgr is
    --To document the length of strings entered.
    Book1 : PhoneBook;
    
+   --Body of Protected Object for contact count
+   
+   protected body ContactCountObj is
+      procedure Incr is
+      begin
+         Is_Ready := False;
+         ContactCount := ContactCount + 1;
+         Is_Ready := True;
+      end Incr;
+      
+      procedure Decr is
+      begin
+         Is_Ready := False;
+         ContactCount := ContactCount - 1;
+         Is_Ready := True;
+      end Decr;
+      
+      entry Get (V : out Natural)
+        when Is_Ready is
+      begin
+         V := ContactCount;
+      end Get;
+   end ContactCountObj;
+   
+   
+   
+   
    --Procedure to Add contact
    procedure Add_Contact is
+      Count : Natural;
+      Index : Natural;
    begin
-      Put_Line ("Adding New Contact..");
+      ContactCountObj.Get(Count);
+      if (Count = 10) then
+         Put_Line("Phone book full. Delete some comtacts");
       
-      -- Read First Name
-      Put_Line ("Enter First Name 15char max");
-      Skip_Line;
-      declare
-         Entered : String := Get_Line;
-       begin
-         Put_Line (Integer'Image(Entered'Length));
-         StrLen := Entered'Length; 
-         Book1(1).FirstName(1 .. StrLen) := Entered ;
-         Book1(1).LenFirstName := StrLen;
-      end;
+      else 
+         Put_Line ("Adding New Contact..");
+         Index := Count + 1;
       
-      --Read Last Name
-      Put_Line ("Enter Last Name 15 char max");
-      --Skip_Line;
-      declare
-         Entered : String := Get_Line;
-       begin
-         Put_Line (Integer'Image(Entered'Length));
-         StrLen := Entered'Length;
-         Book1(1).LastName(1 .. StrLen) := Entered ;
-         Book1(1).LenLastName := StrLen;
-      end;
+         -- Read First Name
+         Put_Line ("Enter First Name 15 char max");
+         Skip_Line;
+         declare
+            Entered : String := Get_Line;
+         begin
+            Put_Line (Integer'Image(Entered'Length));
+            StrLen := Entered'Length; 
+            Book1(Index).FirstName(1 .. StrLen) := Entered ;
+            Book1(Index).LenFirstName := StrLen;
+         end;
       
-      --Read Phone Number
-      Put_Line ("Enter Phone Number 15 char max");
-      --Skip_Line;
-      declare
-         Entered : String := Get_Line;
-       begin
-         Put_Line (Integer'Image(Entered'Length));
-         StrLen := Entered'Length;
-         Book1(1).PhoneNo(1 .. StrLen) := Entered ;
-         Book1(1).LenPhoneNo := StrLen;
-      end;
+         --Read Last Name
+         Put_Line ("Enter Last Name 15 char max");
+         --Skip_Line;
+         declare
+            Entered : String := Get_Line;
+         begin
+            Put_Line (Integer'Image(Entered'Length));
+            StrLen := Entered'Length;
+            Book1(Index).LastName(1 .. StrLen) := Entered ;
+            Book1(Index).LenLastName := StrLen;
+         end;
       
-      -- Fix me: Update Global Contact count
-      Put_Line ("Contact Added at entry 1");
-      Put_Line(Book1(1).FirstName(1.. Book1(1).LenFirstName)
-               & " " & Book1(1).LastName(1.. Book1(1).LenLastName)
-                 & " | " & Book1(1).PhoneNo(1.. Book1(1).LenPhoneNo));      
+         --Read Phone Number
+         Put_Line ("Enter Phone Number 15 char max");
+         --Skip_Line;
+         declare
+            Entered : String := Get_Line;
+         begin
+            Put_Line (Integer'Image(Entered'Length));
+            StrLen := Entered'Length;
+            Book1(Index).PhoneNo(1 .. StrLen) := Entered ;
+            Book1(Index).LenPhoneNo := StrLen;
+         end;
+      
+         -- Fix me: Update Global Contact count
+         Put_Line ("Contact Added at entry " & Natural'Image(Index));
+         Put_Line(Book1(Index).FirstName(1.. Book1(Index).LenFirstName)
+                  & " " & Book1(Index).LastName(1.. Book1(Index).LenLastName)
+                  & " | " & Book1(Index).PhoneNo(1.. Book1(Index).LenPhoneNo));
+      
+         --Incrementing Contact Count
+         ContactCountObj.Incr;
+      end if;
       
     
    end Add_Contact;
