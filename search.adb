684867bfab59303175ae380a92619782a803b17d
SPrasad <s.prasad413@gmail.com>
2019-02-09 22:37:59 +0100

    spark commit

--- search.adb
+++ search.adb
@@ -1,84 +1,58 @@
-with Contact_Mgr;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Text_IO;         use Ada.Text_IO;
+with Contact_Mgr;
 
+package body search
+with SPARK_Mode => On
+is
 
-package body search is
-   --  Create a local copy of updated PhoneBook
-   local_Book : constant Contact_Mgr.PhoneBook := Contact_Mgr.Book1;
-   Count : Natural;
+   function Get_Index (option : in String; value : in String;
+                       searchlen : in Natural; Count : in Natural)
+                       return Integer
+   with SPARK_Mode => On
+   is
+      Indx :  Integer := -1;
+      Finished : Boolean := False;
+      --  Get a local copy of Phonebook
+      local_Book : constant Contact_Mgr.PhoneBook := Contact_Mgr.Book1;
 
-   procedure Search_Contact is
-      SearchOption : Natural;
-      SearchString : String (1 .. 15);
-      Idx : Integer := -1;
-      pragma Unreferenced (Idx);
-      StrLen : Natural;
    begin
-      Put_Line ("Enter your option");
-      Put_Line ("1. Search by Firstname 2. Search by Lastname");
-      Get (SearchOption);
-      case SearchOption is
-         when 1 =>
-            Put_Line ("Enter Firstname");
-            Skip_Line;
-            declare
-               Entered : constant String := Get_Line;
-            begin
-               Put_Line (Integer'Image (Entered'Length));
-               StrLen := Entered'Length;
-               SearchString (1 .. StrLen) := Entered;
-            end;
-            Idx := Get_Index ("Firstname", SearchString);
-
-         when 2 =>
-            Put_Line ("Enter Lastname");
-            declare
-               Entered : constant String := Get_Line;
-            begin
-               Put_Line (Integer'Image (Entered'Length));
-               StrLen := Entered'Length;
-               SearchString (1 .. StrLen) := Entered;
-            end;
-            Idx := Get_Index ("Lasttname", SearchString);
-
-         when others =>
-            Put_Line ("Invalid option");
-      end case;
-   end Search_Contact;
-
-   function Get_Index (option : in String; value : in String)
-                       return Integer is
-      Idx :  Integer := -1;
-      Finished           : Boolean := False;
-   begin
-      Contact_Mgr.ContactCountObj.Get (Count);
-      Put_Line ("Option: " & option);
-      Put_Line ("Value: " & value);
       if (option = "Firstname") then
          for I in  1 .. Count loop
-            if (local_Book (I).FirstName = value) then
-               Idx := I;
+            pragma Assume (local_Book (I).LenFirstName < 16);
+            Indx := -1;
+            if (local_Book (I).FirstName (1 .. local_Book (I).LenFirstName)
+                = value (value'First .. searchlen))
+            then
+               Indx := I;
                Finished := True;
             end if;
             exit when Finished;
+            -- If the control reaches here, 'Finished' has to be false.
+            pragma Assert (Finished = False);
          end loop;
 
       elsif (option = "Lastname") then
-            for I in  1 .. Count loop
-               if (local_Book (I).LastName = value) then
-                  Idx := I;
-                  Finished := True;
-               end if;
-               exit when Finished;
-            end loop;
+         for I in  1 .. Count loop
+            pragma Assume (local_Book (I).LenLastName < 16);
+            Indx := -1;
+            if (local_Book (I).LastName (1 .. local_Book (I).LenLastName)
+                = value (value'First .. searchlen))
+            then
+               Indx := I;
+               Finished := True;
+            end if;
+            exit when Finished;
+            pragma Assert (Finished = False);
+         end loop;
 
       else
+            Indx := -1;
             Put_Line ("Invalid option");
 
       end if;
-      --  Idx should be lesser than or equal to count
-         return Idx;
+      --  pragma Assert (Idx <= Count);
+
+      return Indx;
    end Get_Index;
 
 
