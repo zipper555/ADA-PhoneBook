6d5744851118425cce54a3447e9a647ae8110c2d
SPrasad <s.prasad413@gmail.com>
2019-02-09 22:39:06 +0100

    spark commit

--- time_mgr.ads
+++ time_mgr.ads
@@ -2,15 +2,12 @@ with Ada.Real_Time; use Ada.Real_Time;
 
 package time_mgr is
 
-   --Time statistics
-   StartTime : Time := Clock;
-   LastModified : Time;
-   
+
    task Update_Modified_Time is
       entry Go;
    end Update_Modified_Time;
-   
+
    function Get_Start_Time return Time;
-   
+
    function Get_Last_Modified_Time return Time;
 end time_mgr;
