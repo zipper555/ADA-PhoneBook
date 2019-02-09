82f2a3ade46a76c61cbf3b29dff6f9d8c343de86
SPrasad <s.prasad413@gmail.com>
2019-02-09 22:37:22 +0100

    spark commit

--- contact_mgr.ads
+++ contact_mgr.ads
@@ -1,5 +1,7 @@
 package Contact_Mgr is
 
+   MAX_NO_CONTACTS : constant Integer := 10;
+
    type Contact is record
       FirstName : String (1 .. 15);
       LastName : String (1 .. 15);
@@ -8,11 +10,12 @@ package Contact_Mgr is
       LenLastName : Natural;
       LenPhoneNo : Natural;
    end record;
-   
-   type PhoneBook is array (1..10) of Contact;
-   -- Have declared it here so that it is visible from outside
-   
-   -- Protected Object for contact count
+
+   type PhoneBook is array (1 .. MAX_NO_CONTACTS) of Contact;
+   Book1 : PhoneBook;
+   --  Have declared it here so that it is visible from outside
+
+   --  Protected Object for contact count
    protected ContactCountObj is
       procedure Incr;
       procedure Decr;
@@ -21,12 +24,12 @@ package Contact_Mgr is
        ContactCount  : Natural := 0;
        Is_Ready : Boolean := True;
    end ContactCountObj;
-      
-   
+
+
    procedure Add_Contact;
    procedure Delete_Contact;
-   --procedure Search_Contact;
+   procedure Search_Contact;
    procedure Get_Time_Stats;
-   --procedure Display_PhoneBook;
+   procedure Display_PhoneBook;
 
 end Contact_Mgr;
