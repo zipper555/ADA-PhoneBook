084e1c923e83a5fcc3e1448f16d16c4d8dc03051
SPrasad <s.prasad413@gmail.com>
2019-02-09 22:39:40 +0100

    spark commit

--- time_mgr.adb
+++ time_mgr.adb
@@ -1,34 +1,35 @@
-with Ada.Real_Time; use Ada.Real_Time;
 with Ada.Text_IO;         use Ada.Text_IO;
 
 package body time_mgr is
 
-    --Task to update time stats
-   --Task is chosen for this purpose so that time updation is accurate
-   -- irrespective of other operations.
+   --  Time statistics
+   StartTime : constant Time := Clock;
+   LastModified : Time;
+
+   --  Task to update time stats
+   --  Task is chosen for this purpose so that time updation is accurate
+   --  irrespective of other operations.
    task body Update_Modified_Time is
    begin
-      loop 
-         select 
+      loop
+         select
             accept Go do
-            LastModified := Clock;
-            Put_Line("Last Modified updated");
+               LastModified := Clock;
+               Put_Line ("Last Modified updated");
             end Go;
          or
             terminate;
-         end select;    
-      end loop;   
+         end select;
+      end loop;
    end Update_Modified_Time;
 
    function Get_Start_Time return Time is
    begin
       return StartTime;
    end Get_Start_Time;
- 
+
    function Get_Last_Modified_Time return Time is
    begin
       return LastModified;
    end Get_Last_Modified_Time;
-   
-       
 end time_mgr;
